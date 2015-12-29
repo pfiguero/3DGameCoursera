d1c8ba7d9e112bf2eb49402d4eb94dee454e2e8d
pablofigueroa <pfiguero@me.com>
2015-12-29 07:27:13 -0500

    Primera version de cambio de nivel

---
+++ Assets/Scripts/CambioEscena.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CambioEscena : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter() {
+		Invoke ("Cambiar", 2);
+	}
+
+	void OnTriggerExit() {
+		CancelInvoke ();
+	}
+
+	void Cambiar () {
+		SceneManager.LoadScene ("room-pfiguero");
+	}
+}
