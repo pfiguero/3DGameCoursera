b20cc2696db5388b7bfd56965395e093a672d39b
Calvino85 <and-gome@uniandes.edu.co>
2016-02-05 21:43:15 -0500

    Se agregó pantalla de inicio

---
+++ Assets/Scripts/Personaje/HabilitarAnimaciones.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class HabilitarAnimaciones : MonoBehaviour {
+
+    public Animator animator;
+
+    void Start() {
+
+        animator = this.GetComponent<Animator>();
+
+        if (SceneManager.GetActiveScene().name.Equals("pantallaIntro"))
+        {
+            animator.SetBool("pantallaIntro", true);
+        }
+        else
+        {
+            animator.SetBool("pantallaIntro", false);
+        }
+
+    }
+}
---
+++ Assets/Scripts/UI/CampoNombrePersonaje.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CampoNombrePersonaje : MonoBehaviour {
+
+    public Button botonEmpezar;
+    public InputField campoNombrePersonaje;
+
+    public void cambioTexto() {
+
+        if (!campoNombrePersonaje.text.Trim().Equals(""))
+        {
+            botonEmpezar.interactable = true;
+        }
+        else
+        {
+            botonEmpezar.interactable = false;
+        }
+
+    }
+}
